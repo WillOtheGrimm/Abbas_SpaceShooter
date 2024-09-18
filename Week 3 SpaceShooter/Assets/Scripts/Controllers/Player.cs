a4dcba506b7206354a7e94e4158fead7bc6d08d5
WillOtheGrimm <ayman010905@gmail.com>
2024-09-18 16:06:48 -0400

    Added task 1 a and b

--- Week 3 SpaceShooter/Assets/Scripts/Controllers/Player.cs
+++ Week 3 SpaceShooter/Assets/Scripts/Controllers/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -9,9 +10,85 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+
+    private Vector3 velocity;
+    float shipSpeed = 1f;
+
+    //The amount of time it will take to reach the target speed
+    private float timeToReachSpeed = 3f;
+    //The speed that we want the character to reach after a certain amount of time
+    private float maxSpeed = 10f;
+
+    private float acceleration;
+
+    void Start()
+    {
+        acceleration = maxSpeed / timeToReachSpeed;
+        
+    }
+
     void Update()
     {
 
+        //this line is to make the position go positive on x axis and multiply it by 0.01
+        //transform.position += velocity;
+
+        PlayerMovement();
+
+
+        Debug.Log(shipSpeed);
     }
+
+    void PlayerMovement()
+    {
+
+        velocity = Vector3.zero;
+        
+
+
+        //To limit the speed at max speed
+        if (shipSpeed >= maxSpeed)
+        {
+            shipSpeed = maxSpeed;
+        }
+
+
+
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            velocity += Vector3.down ;
+            shipSpeed += acceleration * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+
+            velocity += Vector3.up;
+            shipSpeed += acceleration * Time.deltaTime;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector3.right;
+            shipSpeed += acceleration * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector3.left;
+            shipSpeed += acceleration * Time.deltaTime;
+
+        }
+
+        transform.position += velocity.normalized * shipSpeed * Time.deltaTime;
+
+    }
+
+
+
 
 }
