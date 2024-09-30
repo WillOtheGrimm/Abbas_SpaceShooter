216f496ee72de7e3e1c73108300255ea0071ba54
WillOtheGrimm <148462140+WillOtheGrimm@users.noreply.github.com>
2024-09-30 18:59:27 -0400

    messing around with the orbit math

--- Week 3 SpaceShooter/Assets/Scripts/Controllers/Moon.cs
+++ Week 3 SpaceShooter/Assets/Scripts/Controllers/Moon.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Moon : MonoBehaviour
 {
     public Transform planetTransform;
     public float orbitSpeed = 50f;
+
+    public float speed;
+
+    Vector3 newPosition;
+
+
+
 
 
     // Start is called before the first frame update
@@ -13,11 +21,27 @@ public class Moon : MonoBehaviour
     {
         
     }
+        int i = 0;
 
     // Update is called once per frame
     void Update()
     {
-        OrbitalMotion(orbitSpeed, planetTransform);
+        //OrbitalMotion(orbitSpeed, planetTransform);
+
+
+        ///////////////////////////////////////////
+
+        // OrbitalSpeed(planetTransform);
+
+        
+
+            newPosition = planetTransform.position + new Vector3(Mathf.Cos(i * Mathf.Deg2Rad * speed) * 2, Mathf.Sin(i * Mathf.Deg2Rad * speed) * 2);
+            transform.position = newPosition;
+            i++;
+        
+         
+
+
     }
 
 
@@ -27,5 +51,13 @@ public class Moon : MonoBehaviour
         transform.RotateAround(target.position, Vector3.back, speed *Time.deltaTime );
     }
 
+
+
+    public void OrbitalSpeed ( Transform target)
+    {
+      
+
+
+    }
 
 }
